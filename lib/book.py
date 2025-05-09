75a6840aa350ac438a5ee6351914b6b1de3abd0d
Kelly Koome <koomekelly7@gmail.com>
2025-05-09 17:46:49 +0300

    Implement Book and Shoe classes with initialization and string representation methods

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, author, year):
+        self.title = title
+        self.author = author
+        self.year = year
+        
+        print(f"Book '{self.title}' by {self.author} ({self.year}) created.")
+        
+    def __repr__(self):
+        return f"Book({self.title!r}, {self.author!r}, {self.year!r})"
+        
+    def __str__(self):
+        return f"{self.title} by {self.author} ({self.year})"
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size, color):
+        self.brand = brand
+        self.size = size
+        self.color = color
+        
+        print(f"Shoe '{self.brand}' of size {self.size} and color {self.color}")
+        
+    def __repr__(self):
+        return f"Shoe({self.brand!r}, {self.size!r}, {self.color!r})"
+        
+    def __str__(self):
+        return f"{self.brand} shoe of size {self.size} in {self.color} color"
+    
